ec49a463b4e5fea4d26fc20ab9fae9ce1a634752
ElAmir-Mansour <xxamirxx00@gmail.com>
2023-08-11 22:51:11 +0300

    Implementation of strategy pattern, payment examples

--- IntroDesignPatterns.playground/Pages/Strategy Pattern.xcplaygroundpage/Contents.swift
+++ IntroDesignPatterns.playground/Pages/Strategy Pattern.xcplaygroundpage/Contents.swift
@@ -1,9 +1,62 @@
-//: [Previous](@previous)
 
-import Foundation
+// first let's define the strategy protocol
+protocol PaymentStrategy {
+    func processPayment(amount: Double)
+}
 
-var greeting = "Hello, playground"
+// implementing the concrete strategies " payment methods "
 
-//: [Next](@next)
+class CreditCardPayment: PaymentStrategy {
+    func processPayment(amount: Double) {
+        print("Processing credit card payment of $\(amount)")
+        // Implement credit card payment logic here
+    }
+}
 
-print("Strategy pattern less goo")
+class PayPalPayment: PaymentStrategy {
+    func processPayment(amount: Double) {
+        print("Processing PayPal payment of $\(amount)")
+        // Implement PayPal payment logic here
+    }
+}
+
+class ApplePayPayment: PaymentStrategy {
+    func processPayment(amount: Double) {
+        print("Processing Apple Pay payment of $\(amount)")
+        // Implement Apple Pay payment logic here
+    }
+}
+
+
+// implementing context class for the client
+
+class PaymentProcessor {
+    private var paymentStrategy: PaymentStrategy
+
+    init(strategy: PaymentStrategy) {
+        self.paymentStrategy = strategy
+    }
+
+    func setPaymentStrategy(strategy: PaymentStrategy) {
+        self.paymentStrategy = strategy
+    }
+
+    func processOrder(amount: Double) {
+        paymentStrategy.processPayment(amount: amount)
+    }
+}
+
+
+// MARK: let's use the strategy
+// Client code
+let creditCardPayment = CreditCardPayment()
+let payPalPayment = PayPalPayment()
+
+let paymentProcessor = PaymentProcessor(strategy: creditCardPayment)
+
+paymentProcessor.processOrder(amount: 100.0)
+
+// Change payment strategy dynamically
+paymentProcessor.setPaymentStrategy(strategy: payPalPayment)
+paymentProcessor.processOrder(amount: 75.0)
+
